75700716d3e86fc9d0cfa5bf3549b8d1f0c2483e
Tharshi <94429043+tharshihecker@users.noreply.github.com>
2025-05-19 11:06:09 +0530

    Update emailService.js

--- ITP1/ITP1/backend/src/utils/emailService.js
+++ ITP1/ITP1/backend/src/utils/emailService.js
@@ -5,8 +5,8 @@ const sendOTPEmail = async (email, otp) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "heckerhackie@gmail.com", // Your Gmail
-        pass: "sobn xxxl ulca limx",     // App password
+        user: "", // Your Gmail
+        pass: "",     // App password
       },
     });
 
